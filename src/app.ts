0771fdcd47e6a1ca219b782a72be52c17de210ff
Siyam <itsyakubsiyam@gmail.com>
2023-06-26 22:16:44 +0600

    Setup globallErrorHandler and Apierror

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 
 const app: Application = express();
 
@@ -10,8 +11,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Testing
-app.get('/', (req: Request, res: Response) => {
-  res.send('Working Successfully');
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  // throw new ApiError(400, 'ore baba error');
+  next('ore baba error');
 });
+
+// global error handler
+app.use(globalErrorHandler);
 
 export default app;
