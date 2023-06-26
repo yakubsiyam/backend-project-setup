2964228a6003932113d02423dffdbf3f2ddc222c
Siyam <itsyakubsiyam@gmail.com>
2023-06-26 21:32:44 +0600

    Setup basic logger using winston

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import mongoose from 'mongoose';
 import config from './config/index';
 import app from './app';
+import { errorlogger, logger } from './shared/logger';
 
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string);
-    console.log('Database is connected successfully');
+    logger.info(`🛢   Database is connected successfully`);
 
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`);
+      logger.info(`Application  listening on port ${config.port}`);
     });
-  } catch (error) {
-    console.log('Failed to connect', error);
+  } catch (err) {
+    errorlogger.error('Failed to connect database', err);
   }
 }
 
