88bd2b257d0ac55d02ccbcff3eb5ad7c2f77f697
Siyam <itsyakubsiyam@gmail.com>
2023-06-27 22:10:02 +0600

    Detect unhandled rejection

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
 import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
@@ -11,9 +13,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Testing
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  // throw new ApiError(400, 'ore baba error');
-  next('ore baba error');
+// app.get('/', (req: Request, res: Response, next: NextFunction) => {
+//   // throw new ApiError(400, 'ore baba error');
+//   next('ore baba error');
+// });
+
+app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+  Promise.reject(new Error('Unhandled Promise Rejection'));
 });
 
 // global error handler
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,34 @@
+/* eslint-disable no-console */
 import mongoose from 'mongoose';
 import config from './config/index';
 import app from './app';
 import { errorlogger, logger } from './shared/logger';
+import { Server } from 'http';
 
 async function bootstrap() {
+  let server: Server;
   try {
     await mongoose.connect(config.database_url as string);
     logger.info(`🛢   Database is connected successfully`);
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       logger.info(`Application  listening on port ${config.port}`);
     });
   } catch (err) {
     errorlogger.error('Failed to connect database', err);
   }
+
+  process.on('unhandledRejection', error => {
+    console.log('Unhandled Rejection is detected, we are closing');
+    if (server) {
+      server.close(() => {
+        errorlogger.error(error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
 }
 
 bootstrap();
